6f4b9f0705e9eadf29e19af4add886b5baa960b9
1746924261 <54863160+1746924261@users.noreply.github.com>
2020-10-09 04:10:23 -0400

    added CLICOLOR env variable

--- src/CheckURL.js
+++ src/CheckURL.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
+require('dotenv').config();
 const fs = require('fs');                   //used for file reading
 const request = require('request');         //module used for making GET reqeusts for status code response
 const chalk = require('chalk');             //module used for output colors
 const args = process.argv.slice(2);         //using slice function to get rid of the 2 default args
 const regex = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,10}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g); //RegEx to extract urls
+
+if(process.env.CLICOLOR == 0){
+    chalk.level = 0;
+}
 
 if(args.length == 0){                       //displaying parameter options if no parameter is used
     missingParams();
