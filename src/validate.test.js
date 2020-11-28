8cc0ddf579b90e0d6972afa7ac226b5e673adfe2
1746924261 <54863160+1746924261@users.noreply.github.com>
2020-11-27 21:56:43 -0500

    added validComment tests

--- src/validate.test.js
+++ src/validate.test.js
@@ -1,4 +1,4 @@
-const { validProtocol } = require('./validate');
+const { validProtocol, validComment } = require('./validate');
 
 describe('ignore url tests', () => {
   test('url with http or https protocol should return true', () => {
@@ -9,5 +9,20 @@ describe('ignore url tests', () => {
   test('url with non-http and non-https protocol should return false', () => {
     const link = 'www.randomurl.com';
     expect(validProtocol(link)).toBe(false);
+  });
+
+  test('empty line should result the validComment to return false', () => {
+    const emptyLine = '';
+    expect(validComment(emptyLine)).toBe(false);
+  });
+
+  test(`line which does not start with '#' should result the validComment to return false`, () => {
+    const invalidLine = 'random text that does not start with #';
+    expect(validComment(invalidLine)).toBe(false);
+  });
+
+  test(`line which start with '#' should result the validComment to return true`, () => {
+    const validLine = '#random text that starts with #';
+    expect(validComment(validLine)).toBe(true);
   });
 });
